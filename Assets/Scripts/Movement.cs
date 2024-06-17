c1b677d2b25e8c416907d915d8484c84393f1111
Ruby Osborn <rubyosborn@proton.me>
2024-06-17 13:50:15 +0100

    Player movement scripts

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,17 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+namespace CharacterMovement
 {
-    // Start is called before the first frame update
-    void Start()
+    ///<summary>
+    ///Generic movement methods which can be applied to any entity with a rigidbody
+    ///Logic for detecting entity states goes here, controllers shouldn't be doing any logic regarding movement i.e. ground check, movement speed e.c.t.
+    ///</summary>
+    public class Movement : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private float _moveSpeed;
+        [SerializeField] private float _jumpHeight;
+        [SerializeField] private Rigidbody2D _rigidbody;
+        [SerializeField] private Transform _groundedCheckPosition;
+        [SerializeField] private Vector2 _groundCheckDimensions;
+        [SerializeField] private LayerMask _groundLayer;
+        private bool _grounded;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        //OnValidate will automatically run and populate the rigidbody field
+        //This method has no major performance impact as it is editor-only
+        private void OnValidate()
+        {
+            if (!_rigidbody)
+            {
+                _rigidbody = GetComponent<Rigidbody2D>();
+            }
+        }
+
+        //This will come from keyboard for this game, so values for WASD will be -1 to 1, used to determine more direction
+        public void Move(Vector2 movementInput)
+        {
+            _rigidbody.velocity = new Vector2(movementInput.x * _moveSpeed, _rigidbody.velocity.y);
+        }
+
+        public void Jump()
+        {
+            if (_grounded)
+            {
+                _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _jumpHeight);
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (Physics2D.OverlapBox(_groundedCheckPosition.position, _groundCheckDimensions, 0f, _groundLayer))
+            {
+                _grounded = true;
+            }
+            else
+            {
+                _grounded = false;
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireCube(_groundedCheckPosition.position, _groundCheckDimensions);
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace CharacterMovement
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerController : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Movement _movement;
+        private Vector2 _movementInput;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Update()
+        {
+            //GetKeyDown is not the best input method, input manager package is better and more flexible
+            //But keeping it simple for this project due to short timeframe
+
+            //GetKeyDown means if two keys are held down at the same time, the last pressed one will be used
+            //Being pressed at the same frame is unlikely and we don't care so much what happens in this case, as long as an input registers
+            if (Input.GetKey(KeyCode.A))
+            {
+                _movementInput.x = -1f;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                _movementInput.x = 1f;
+            }
+            else
+            {
+                _movementInput.x = 0f;
+            }
+
+            _movement.Move(_movementInput);
+
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                _movement.Jump();
+            }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                //Dialogue start
+            }
+        }
     }
 }
