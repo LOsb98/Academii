52eef9f7d7bc2c053d7e41ea7e4b28231a285836
Ruby Osborn <rubyosborn@proton.me>
2024-06-18 20:29:41 +0100

    New window to show on game finish

--- Assets/Scripts/MainUIManager.cs
+++ Assets/Scripts/MainUIManager.cs
@@ -8,10 +8,16 @@ namespace MainUI
     public class MainUIManager : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI _objectiveText;
+        [SerializeField] private GameObject _finishedWindow;
 
         public void UpdateObjective(string newObjective)
         {
             _objectiveText.text = $"OBJECTIVE: {newObjective}";
+        }
+
+        public void ShowFinishedWindow()
+        {
+            _finishedWindow.SetActive(true);
         }
     }
 }
